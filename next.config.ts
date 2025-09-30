18db2981aa9ab2ef237a02afe031bfb15e1809cd
shaan <darknetnylex22@gmail.com>
2025-09-30 11:30:10 +1000

    git push for pages deployment reactivation with a image fix

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,15 @@ let isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/Formula1-website" : "",        // your repo name
-  assetPrefix: isProd ? "/Formula1-website/" : "",    // same as basePath
-  trailingSlash: true,                  // ensures deep pages become index.html
-  images: { unoptimized: true },
+  basePath: isProd ? "/Formula1-website" : "", // your repo name
+  assetPrefix: isProd ? "/Formula1-website/" : "", // same as basePath
+  trailingSlash: true, // ensures deep pages become index.html
+  images: { unoptimized: true, remotePatterns: [
+    {
+      protocol: "http",
+      hostname: "shaan11obe.github.io/Formula1-Website", // Replace with your image host
+    },
+  ], }
 };
 
 export default nextConfig;
