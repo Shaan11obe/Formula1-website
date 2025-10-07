7ec5c4a71aff1aa5b823d99df9726d14b3c78363
shaan <darknetnylex22@gmail.com>
2025-10-07 14:45:02 +1100

    yay

--- src/app/circuits/page.tsx
+++ src/app/circuits/page.tsx
@@ -3,9 +3,14 @@ import TrackGrid from "@/components/TrackGrid"
 
 const TeamsPage = () => {
   const images = [
-    "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&auto=format&fit=crop&w=2560&q=80",
-    "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&auto=format&fit=crop&w=2940&q=80",
-    "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&auto=format&fit=crop&w=2762&q=80",
+    "/tracks/ALBERT_PARK.jpg",
+    "/tracks/SHANGHAI.jpg",
+    "/tracks/SUZUKA.jpg",
+    "/tracks/BAHRAIN.jpg",
+    "/tracks/JEDDAH.jpg",
+    "/tracks/MIAMI.jpg",
+    "/tracks/IMOLA.jpg",
+    "/tracks/MONACO.jpg",
   ];
 
   return (
--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -3,9 +3,12 @@ import CarGrid from "@/components/CarGrid";
 
 const TeamsPage = () => {
   const images = [
-    "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&auto=format&fit=crop&w=2560&q=80",
-    "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&auto=format&fit=crop&w=2940&q=80",
-    "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&auto=format&fit=crop&w=2762&q=80",
+    "/mclaren-logo.png",
+    "/red-bull-logo.png",
+    "/mercedes-logo.png",
+    "/ferrai-logo.png",
+    "/haas-logo.png",
+    "/racingbulls-logo.png",
   ];
 
   return (
